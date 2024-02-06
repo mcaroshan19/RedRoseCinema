b29e06d02f6bc59a8fffc241eb23660e7c9c2018
Roshan <mcaroshan2@gmail.com>
2024-02-06 23:28:32 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,58 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { FilmsComponent } from './films/films.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { OurWorkComponent } from './our-work/our-work.component';
+import { HomeComponent } from './home/home.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+
+
+ 
+
+const appRoute: Routes=[
+  {path:'home', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'films', component: FilmsComponent},
+  {path:'gallery', component: GalleryComponent},
+  {path:'our-work', component: OurWorkComponent},
+  
+ 
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    AboutComponent,
+    FilmsComponent,
+    GalleryComponent,
+    OurWorkComponent,
+    HomeComponent,
+    FooterComponent
   ],
+
   imports: [
+   
     BrowserModule,
+    [RouterModule.forRoot(appRoute)],
+    FormsModule,
+    
     AppRoutingModule
+
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/our-work/our-work.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-work',
+  templateUrl: './our-work.component.html',
+  styleUrls: ['./our-work.component.css']
+})
+export class OurWorkComponent {
+
+}
